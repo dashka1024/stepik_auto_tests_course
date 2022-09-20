56ab3f219d899ccd87a07437a06b4ba9bba1d29a
dashka1024 <d.baranovskaya1024@gmail.com>
2022-09-20 11:37:24 +0300

    languages+

--- test_items.py
+++ test_items.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.common.by import By
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_should_see_add_button(browser):
     browser.get(link)
+    time.sleep(30)
     x=browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert x is not None, "Кнопки нет"
